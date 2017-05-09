80a8419de14d7c74ebc4cf4f57427b8baedca3f1
Igor Pires Ferreira <ipferreira@inf.ufrgs.br>
2017-05-08 21:03:52 -0300

    Arrumando loop do dispatcher

--- src/main2.c
+++ src/main2.c
@@ -16,55 +16,51 @@ Igor Pires Ferreira - 242267
 
 #define TEM_THREAD_NA_FILA 0
 #define NAO_TEM_THREAD_NA_FILA 1
+#define stackLength SIGSTKSZ
 
 FILA2 fila;
-char pilhas[4][SIGSTKSZ];
-ucontext_t mainContext, mainContext2;
+char pilha[stackLength];
+ucontext_t mainContext, contextDispatcher;
+TCB_t* currentTCB;
 
-void PA(ucontext_t* this, ucontext_t *next) {
-    printf("PA ");
+void PA() {
     int i, r = 4, n = 0;
     for (i = 1; i <= NUM_PA; ++i) {
+        printf("PA ");
         n+=r;
         printf("i: %d n: %d\n", i, n);
-        swapcontext(this, next);
-        printf("HAY\n");
-        // setcontext(next);
+        swapcontext(&currentTCB->context, &contextDispatcher);
     }
 }
-void PG(ucontext_t* this, ucontext_t *next) {
-    printf("PG ");
+void PG() {
     int i, r = 2, n = 1;
     for (i = 1; i <= NUM_PG; ++i) {
+        printf("PG ");
         n*=r;
         printf("i: %d n: %d\n", i, n);
-        swapcontext(this, next);
+        swapcontext(&currentTCB->context, &contextDispatcher);
     }
 }
-void fibonacci(ucontext_t* this, ucontext_t *next) {
+void fibonacci() {
     int f, f0 = 1, f1 = 1, i;
-printf("fib  ");
     for (i = 2; i <= NUM_FIBONACCI; ++i)    {       
+        printf("fib  ");
         f = f0 + f1;
         f0 = f1;
         f1 = f; 
         printf("i: %d f: %d\n", i, f);
-        swapcontext(this, next);
-        // printf("i: %d, f: %d\n", i, f);
+        swapcontext(&currentTCB->context, &contextDispatcher);
     }
 }
-void triangulo(ucontext_t* this, ucontext_t *next) {
+void triangulo() {
     int t = 0, i;
-printf("tri ");
     for (i = 1; i <= NUM_TRIANGULO; i++) {      
+        printf("tri ");
         t += (i-1);
         printf("i: %d t: %d\n", i, t);
-        swapcontext(this, next);
-        // printf("i: %d, t: %d\n", i, t);
+        swapcontext(&currentTCB->context, &contextDispatcher);
     }
 }
-
-
 
 int tamanhoFila(FILA2 *fila) {
     int resultado = FirstFila2(fila);
@@ -82,19 +78,23 @@ int tamanhoFila(FILA2 *fila) {
     } 
     return tamanho;
 }
-
-void cedeuCPU(){
-
-    TCB_t* TCBdaThreadNaCPU = (TCB_t *) GetAtIteratorFila2(&fila);
-    getcontext(&(TCBdaThreadNaCPU->context));  
-    DeleteAtIteratorFila2(&fila);
-    AppendFila2(&fila, (void *) TCBdaThreadNaCPU);
-    TCB_t* TCBdaNovaThreadNaCPU = (TCB_t *) GetAtIteratorFila2(&fila);
-    setcontext(&(TCBdaNovaThreadNaCPU->context));
-
-    //minha ideia era fazer essa func aqui pra rodar a cada iteracao das funcs matem'aticas, e ao final de cada uma,
-    //elas chamarem um fimExecthread que retira a thread da fila e seta o contexto pra main se for a ultima 
-    //thread ainda em exec
+void clear() {
+    free(currentTCB->context.uc_stack.ss_sp);
+    free(currentTCB);
+    currentTCB = NULL;
+}
+void terminate() {
+    clear();
+    setcontext(&contextDispatcher);
+}
+void dispatch(){
+    if(!FirstFila2(&fila)) {
+        currentTCB = GetAtIteratorFila2(&fila);
+        DeleteAtIteratorFila2(&fila);
+        AppendFila2(&fila, (void *) currentTCB);
+        printf("tcb: %d ", currentTCB->tid);
+        setcontext(&currentTCB->context);
+    }
     
 }
 
@@ -102,7 +102,7 @@ int main (int argc, char** argv) {
     void* funcs[4] = {PA, PG, fibonacci, triangulo}; 
     
     
-    int criou = CreateFila2(&fila);
+    int criou = CreateFila2(&fila), ret_code = 1;
     if(criou!=0){
         printf("Erro ao criar a fila \n");      
         exit(1);
@@ -114,10 +114,10 @@ int main (int argc, char** argv) {
     for(i = 0 ; i < 4; i++) {        
         ucontext_t context;
         getcontext(&context);
-        context.uc_stack.ss_sp = pilhas[i];
-        context.uc_stack.ss_size = sizeof(pilhas[i]);
+        context.uc_stack.ss_sp = (char *) malloc(stackLength);
+        context.uc_stack.ss_size = stackLength;
         context.uc_link =  &mainContext;
-        makecontext(&context, (void (*)(void))funcs[i], 2, &context, &mainContext);
+        makecontext(&context, (void (*)(void))funcs[i],0);
 
         TCB_t* novoTCB = malloc(sizeof(TCB_t));
         novoTCB->tid = i;
@@ -126,21 +126,36 @@ int main (int argc, char** argv) {
     }
 
     
-    //dispatcher    
-    while(tamanhoFila(&fila)) {
-        // printf("s ");
-        TCB_t* tcb; 
-        getcontext(&mainContext);
-        if(!FirstFila2(&fila)) {
-            tcb = GetAtIteratorFila2(&fila);
-            DeleteAtIteratorFila2(&fila);
-            AppendFila2(&fila, (void *) tcb);
-            printf("tcb: %d ", tcb->tid);
-            swapcontext(&mainContext2, &tcb->context);            
-            // printf("s2 ");
-        }
-
+    //dispatcher
+    getcontext(&contextDispatcher);
+    contextDispatcher.uc_stack.ss_sp = (char *) malloc(stackLength);     
+    contextDispatcher.uc_stack.ss_size = stackLength;
+    contextDispatcher.uc_link = 0;
+    makecontext(&contextDispatcher, (void (*)(void))dispatch, 0);     
+    getcontext(&mainContext);
+    if(ret_code){        
+        ret_code = 0;
+        setcontext(&contextDispatcher);
     }
+    
+
+    // printf("hay\n");
+    // if(ret_code) {
+    //     ret_code = 0;
+    //     while(tamanhoFila(&fila)) {
+    //         TCB_t* tcb; 
+    //         if(!FirstFila2(&fila)) {
+    //             tcb = GetAtIteratorFila2(&fila);
+    //             DeleteAtIteratorFila2(&fila);
+    //             printf("tcb: %d ", tcb->tid);
+    //             swapcontext(&mainContext,&tcb->context);            
+    //             AppendFila2(&fila, (void *) tcb);
+    //             printf("s2 ");
+    //         }
+
+    //     }    
+    // }
+    
 
     return 0;
 }
